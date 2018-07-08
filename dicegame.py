a46a0482e654f646b43afeff211065c083b5f4ed
0083 <s1250083@u-aizu.ac.jp>
2018-07-08 23:18:12 +0900

    initial revision

---
+++ dicegame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from numpy.random import *
+
+print 'Rolling the dice...'
+
+die1 = randint(1,7)
+die2 = randint(1,7)
+
+print 'Die 1:',die1
+print 'Die 2:',die2
+
+print 'Total value:',die1 + die2
